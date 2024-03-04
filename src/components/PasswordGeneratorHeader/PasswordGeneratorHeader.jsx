009e94ab70baad858027bb625cce0464baf0a632
Favio V <favio102@protonmail.com>
2024-03-04 17:54:46 +0900

    header added

--- src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
+++ src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
@@ -1,5 +1,11 @@
 import s from "./style.module.css";
+import lockIcon from "../../assets/lock.svg";
 
 export function PasswordGeneratorHeader() {
-    return <div>Password Generator Header</div>
+  return (
+    <div className={s.root}>
+      <div className={s.title}>Password Generator</div>
+      <img src={lockIcon} alt="lockIcon" className={s.icon}/>
+    </div>
+  );
 }
