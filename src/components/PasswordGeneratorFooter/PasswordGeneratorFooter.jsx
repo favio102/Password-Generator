5bc93bcde4e267f9e386b30ceef08a4bc763a074
Favio V <favio102@protonmail.com>
2024-03-05 14:22:23 +0900

    footer styled

--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,5 +1,11 @@
 import s from "./style.module.css";
+import copyPaste from "../../assets/copy-paste.svg"
 
 export function PasswordGeneratorFooter() {
-    return <div className={s.root}>Password Generator Footer</div>
+  return (
+    <div className={s.root}>
+      <div>super password</div>
+      <img className={s.icon} src={copyPaste} alt="copy and paste" />
+    </div>
+  );
 }
