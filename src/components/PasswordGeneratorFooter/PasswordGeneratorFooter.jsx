6130fe005059fac9ade20cf11669b79b5455b44a
Favio V <favio102@protonmail.com>
2024-03-06 14:23:27 +0900

    navigator clipboard writeText added to copy and paste feature

--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -2,8 +2,12 @@ import s from "./style.module.css";
 import copyPaste from "../../assets/copy-paste.svg";
 
 export function PasswordGeneratorFooter(props) {
+  function saveInClipboard() {
+    navigator.clipboard.writeText(props.password);
+  }
+
   return (
-    <div className={s.root}>
+    <div className={s.root} onClick={saveInClipboard}>
       <div>{props.password}</div>
       <img className={s.icon} src={copyPaste} alt="copy and paste" />
     </div>
